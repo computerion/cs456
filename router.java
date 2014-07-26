df5e435e7c948d98adf14c8c6d6825c79414a986
Daniel Hu <daniel@Hengs-MacBook-Pro.local>
2014-07-26 10:58:07 -0400

    bullshit

--- router.java
+++ router.java
@@ -7,7 +7,7 @@ public class router {
 
 	public static final int NBR_ROUTER = 5;
 	private static int routerId;
-	private static link_cost[] linkCosts;
+	private static link[] links;
 
 	private static DatagramSocket socket;
 	private static int routerPort;
@@ -38,42 +38,59 @@ public class router {
 	  }
 
 	private static void sendInit() throws Exception {
-		ByteBuffer buffer = ByteBuffer.allocate(4);
-		buffer.order(ByteOrder.LITTLE_ENDIAN);
-		buffer.putInt(routerId);
-    	DatagramPacket sendPacket = new DatagramPacket(buffer.array(), 4, hostAddress, hostPort); 
-    	System.out.println("Sending"); 
+		pkt_INIT pkt = new pkt_INIT(routerId);
+    	DatagramPacket sendPacket = new DatagramPacket(pkt.toByte(), 4, hostAddress, hostPort); 
     	socket.send(sendPacket);
-    	System.out.println("Sent Packets");
 	}
 
 	private static void recieveCircuitDB() throws Exception {
 		byte[] data = new byte[512];
 	    DatagramPacket receivePacket = new DatagramPacket(data, data.length);  
-	    System.out.println("Recieving");
 	    socket.receive(receivePacket);
-	    ByteBuffer buffer = ByteBuffer.wrap(receivePacket.getData());
-	    buffer.order(ByteOrder.LITTLE_ENDIAN);
-	    int numLinks = buffer.getInt();
-	    System.out.println(numLinks);
-	    linkCosts = new link_cost[numLinks];
-	    for (int i=0; i<numLinks; i++) {
-	    	linkCosts[i] = new link_cost();
-	    	linkCosts[i].link = buffer.getInt();
-	    	linkCosts[i].cost = buffer.getInt();
-	    	System.out.println(linkCosts[i].link + " " + linkCosts[i].cost);
-	    }
+	    circuit_DB circutDB = circuit_DB.getData(receivePacket.getData());
+	}
+
+	private static void sendHellos() throws Exception {
+		for (int i=0; i<links.length; i++) {
+			
+		}
 	}
 }
 
 class link {
-	public link_cost link;
 	public int reciever_router_id;
+	public int link_id;
+	public int cost;
+
+	public link(link_cost lc) {
+		link_id = lc.link;
+		cost = lc.cost;
+	}
 }
 
 class pkt_HELLO { 
 	public int router_id; /* id of the router who sends the HELLO PDU */ 
-	public int link_id; /* id of the link through which it is sent */ 
+	public int link_id; /* id of the link through which it is sent */
+
+	public pkt_HELLO(int router_id, int link_id) {
+		this.router_id = router_id;
+		this.link_id = link_id;
+	}
+
+	public byte[] toByte() {
+		ByteBuffer buffer = ByteBuffer.allocate(8);
+		buffer.putInt(router_id);
+		buffer.putInt(link_id);
+		return buffer.array();
+	}
+	
+	public static pkt_HELLO getData(byte[] UDPdata) throws Exception {
+		ByteBuffer buffer = ByteBuffer.wrap(UDPdata);
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		int router_id = buffer.getInt();
+		int link_id = buffer.getInt();
+		return new pkt_HELLO(router_id, link_id);
+	}
 } 
 
 class pkt_LSPDU { 
@@ -86,15 +103,58 @@ class pkt_LSPDU {
 
 class pkt_INIT { 
 	public int router_id; /* id of the router that send the INIT PDU */
+
+	public pkt_INIT(int router_id) {
+		this.router_id = router_id;
+	}
+
+	public byte[] toByte() {
+		ByteBuffer buffer = ByteBuffer.allocate(4);
+		buffer.putInt(router_id);
+		return buffer.array();
+	}
+	
+	public static pkt_INIT getData(byte[] UDPdata) throws Exception {
+		ByteBuffer buffer = ByteBuffer.wrap(UDPdata);
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		int router_id = buffer.getInt();
+		return new pkt_INIT(router_id);
+	}
 } 
 
 class link_cost { 
 	public int link; /* link id */ 
-	public int cost; /* associated cost */ 
+	public int cost; /* associated cost */
+
+	public link_cost(int link, int cost) {
+		this.link = link;
+		this.cost = cost;
+	}
 } 
 
 class circuit_DB { 
 	public int nbr_link; /* number of links attached to a router */ 
-	public link_cost[] linkcost; 
-/* we assume that at most NBR_ROUTER links are attached to each router */
+	public link_cost[] linkcost; /* we assume that at most NBR_ROUTER links are attached to each router */
+
+	public circuit_DB(int nbr_link, link_cost[] linkcost) {
+		this.nbr_link = nbr_link;
+		this.linkcost = linkcost;
+	}
+
+	public static circuit_DB getData(byte[] UDPdata) throws Exception {
+		ByteBuffer buffer = ByteBuffer.wrap(UDPdata);
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+
+	    int nbr_link = buffer.getInt();
+	    link_cost[] linkcost = new link_cost[nbr_link];
+
+	    for (int i=0; i < nbr_link; i++) {
+	    	int link_id = buffer.getInt();
+	    	int cost = buffer.getInt();
+	    	linkcost[i] = new link_cost(link_id, cost);
+	    	System.out.println(linkcost[i].link + " " + linkcost[i].cost);
+	    }
+
+	    return new circuit_DB(nbr_link, linkcost);
+	}
 } 
